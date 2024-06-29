b26975580178990f3507038094515c6dbd5b584c
Juma <jumakymyshbekuulu@gmail.com>
2024-06-29 14:51:08 +0600

    add item

---
+++ index.js
@@ -0,0 +1,6 @@
+let item = document.querySelector(".item")
+let box = document.querySelector(".box")
+function checkTasck(){
+    item.computedStyleMap.borderRadius = "20px 20px 100px 100px"
+}
+// box
